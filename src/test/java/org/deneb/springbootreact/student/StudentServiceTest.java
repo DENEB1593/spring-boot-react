f1310a36d9048289006609b3dc6d7c807aaf6cb7
deneb1593 <deneb1593@gmail.com>
2022-10-29 19:19:17 +0900

    Email 유효성 단위테스트 추가

--- src/test/java/org/deneb/springbootreact/student/StudentServiceTest.java
+++ src/test/java/org/deneb/springbootreact/student/StudentServiceTest.java
@@ -1,15 +1,23 @@
 package org.deneb.springbootreact.student;
 
+import org.deneb.springbootreact.student.exception.BadRequestException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static java.util.Optional.of;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -17,13 +25,8 @@ class StudentServiceTest {
 
   @Mock
   private StudentRepository studentRepository;
+  @InjectMocks
   private StudentService studentService;
-
-  @BeforeEach
-  void setUp() {
-    studentService = new StudentService(studentRepository);
-  }
-
 
 
   @Test
@@ -50,9 +53,25 @@ class StudentServiceTest {
 
     verify(studentRepository).save(studentArgumentCaptor.capture());
 
-    Student capturedStudent = studentArgumentCaptor.capture();
+    Student capturedStudent = studentArgumentCaptor.getValue();
 
     assertThat(capturedStudent).isEqualTo(student);
+  }
+
+  @Test
+  @DisplayName("학생_이메일_존재시")
+  void ifEmailExistWhenSave() {
+    // given
+    Student student = new Student(1L, "Kim", "Kim@email.com", Gender.MALE);
+    given(studentRepository.findByEmail(anyString())).willReturn(of(student));
+
+    assertThatThrownBy(() -> studentService.save(student))
+      .isInstanceOf(BadRequestException.class)
+      .hasMessageContaining(student.getEmail() + " already exists");
+
+
+    // save() 기능을 사용하지 않음을 명시
+    verify(studentRepository, never()).save(any());
   }
 
   @Test
